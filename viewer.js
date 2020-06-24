da136607615ee9a82ea574e15a5b97b8ea12a1ae
thsconline <thsconline@users.noreply.github.com>
2020-06-25 00:51:06 +1000

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -87,67 +87,59 @@ var version;
 // https://stackoverflow.com/questions/19327749/javascript-blob-filename-without-link
 function downloadFile(myobject)
 {
-	var fileData =myobject.fileref;
+	try
+	{
+
+	var id =myobject.fileref;
+	var fileData =myobject.data;
+
 	var filename = myobject.name;
-	
+
 	const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
-  const byteCharacters = atob(b64Data);
-  const byteArrays = [];
+		const byteCharacters = atob(b64Data);
+		const byteArrays = [];
 
-  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    const slice = byteCharacters.slice(offset, offset + sliceSize);
+		for (let offset = 0; offset < byteCharacters.length; offset += sliceSize)
+		{
+			const slice = byteCharacters.slice(offset, offset + sliceSize);
+			const byteNumbers = new Array(slice.length);
+			for (let i = 0; i < slice.length; i++)
+			{
+				byteNumbers[i] = slice.charCodeAt(i);
+			}
 
-    const byteNumbers = new Array(slice.length);
-    for (let i = 0; i < slice.length; i++) {
-      byteNumbers[i] = slice.charCodeAt(i);
-    }
+			const byteArray = new Uint8Array(byteNumbers);
+			byteArrays.push(byteArray);
+		}
+		const blob = new Blob(byteArrays, {type: contentType});
+		return blob;
+	}
+	const blob = b64toBlob(fileData, "application/pdf");
+	//const blobUrl = URL.createObjectURL(blob);
 
-    const byteArray = new Uint8Array(byteNumbers);
-    byteArrays.push(byteArray);
-  }
+	 if (window.navigator.msSaveOrOpenBlob) {
+	    window.navigator.msSaveOrOpenBlob(blob, filename);
+	  } else {
+	    const a = document.createElement('a');
+	    document.body.appendChild(a);
+	    const url = window.URL.createObjectURL(blob);
+	    a.href = url;
+	    a.download = filename;
+	    a.click();
+	    setTimeout(() => {
+	      window.URL.revokeObjectURL(url);
+	      document.body.removeChild(a);
+	    }, 0)
+	  }	
 
-  const blob = new Blob(byteArrays, {type: contentType});
-  return blob;
+	window.location = blobUrl;
+	}
+	catch(err)
+	{
+	var b="https://drive.google.com/uc?export=download&id="+id;
+	window.location = b;
+	}
 }
-const blob = b64toBlob(fileData, "application/pdf");
-//const blobUrl = URL.createObjectURL(blob);
-
- if (window.navigator.msSaveOrOpenBlob) {
-    window.navigator.msSaveOrOpenBlob(blob, filename);
-  } else {
-    const a = document.createElement('a');
-    document.body.appendChild(a);
-    const url = window.URL.createObjectURL(blob);
-    a.href = url;
-    a.download = filename;
-    a.click();
-    setTimeout(() => {
-      window.URL.revokeObjectURL(url);
-      document.body.removeChild(a);
-    }, 0)
-  }	
-	
-	
-window.location = blobUrl;
-	
-	
-/*    const downloadLink = document.createElement("a");
-
-    downloadLink.href = "data:application/pdf;base64"+fileData;
-    downloadLink.download = filename;
-	document.body.appendChild(downloadLink);
-    downloadLink.click();
-	document.body.removeChild(downloadLink);
-	
-//*/	var b="https://drive.google.com/uc?export=download&id="+fileData;
-	
-/*	
-//*/	window.location = b;
-
-	
-}
-
-
 
 function loadpage()
 {
