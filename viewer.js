5dbe8771b3171dc6445d11b41fb76ee584c76828
thsconline <thsconline@users.noreply.github.com>
2020-06-25 00:36:31 +1000

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -87,6 +87,48 @@ function downloadFile(myobject)
 {
 	var fileData =myobject.fileref;
 	var filename = myobject.name;
+	
+	const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  const blob = new Blob(byteArrays, {type: contentType});
+  return blob;
+}
+const blob = b64toBlob(fileData, "application/pdf");
+//const blobUrl = URL.createObjectURL(blob);
+
+ if (window.navigator.msSaveOrOpenBlob) {
+    window.navigator.msSaveOrOpenBlob(blob, filename);
+  } else {
+    const a = document.createElement('a');
+    document.body.appendChild(a);
+    const url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = filename;
+    a.click();
+    setTimeout(() => {
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(a);
+    }, 0)
+  }	
+	
+	
+window.location = blobUrl;
+	
+	
 /*    const downloadLink = document.createElement("a");
 
     downloadLink.href = "data:application/pdf;base64"+fileData;
