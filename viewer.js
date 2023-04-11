c7f5569d078dcb06c372c653401daf392f6931a8
thsconline <thsconline@users.noreply.github.com>
2023-04-12 00:01:31 +1000

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -275,8 +275,8 @@ function pdf(input, viewno)
   
     var titlex = input.innerHTML;
     var b = viewno+titlex
-    var vx = (SHA256(b.substring(20))).substring(6)
-    
+    var vy = SHA256(b.substring(20))
+    var vx = vy.substring(6) 
 /*	
 	
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
