48e07def79b140ef55051c6e263fdadc1b3e6e62
dp964 <thsconline@gmail.com>
2023-07-12 23:56:46 +1000

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -19,11 +19,11 @@ function loadshell()
 {
 	var url = window.location.pathname;
 	var url = url.replace("&", "_");	
-	var url = url.replace("/v1", "/v/1");
+/*	var url = url.replace("/v1", "/v/1");
 	var url = url.replace("/v2", "/v/2");
 	var url = url.replace("/v3", "/v/3");
 	var url = url.replace("/v6", "/v/6");
-	var url = url.replace("/v7", "/v/7");	
+	var url = url.replace("/v7", "/v/7");	*/
 	var queryx = url.split("/s/")[1].split("/")[0]
 		
 	switch(queryx)
@@ -186,17 +186,17 @@ function loadshell()
 		try
 		{
 			
-			var hash = url.split("/s/v/")[1].split("/")[0]
+			//var hash = url.split("/s/v/")[1].split("/")[0]
 			var viewno = url.split("/s/v/")[1].split("/")[1]
 			var titlex = url.split("/s/v/")[1].split("/")[2]
-		    	var vb = viewno+btoa(titlex.replace(/\ /g, "_").substring(0,4))
+		    	/*var vb = viewno+btoa(titlex.replace(/\ /g, "_").substring(0,4))
 		    	var vy = SHA256(vb.trim())
     			var vx = vy.toUpperCase().charCodeAt(0)-32
 			if(hash != vx){
 				document.write("Unable to connect to viewer")
 			}
 			else
-			{
+			{*/
 			
 				var redirecturl = "https://thsconline.github.io/s/v/" + url.split("/s/v/")[1]
 				if(window.self !== window.top)
@@ -219,7 +219,7 @@ function loadshell()
 				win.document.write("<iframe style=\"width:100%; height:96%;\" height=\"96%\" sandbox=\"allow-scripts allow-popups allow-pointer-lock allow-presentation allow-same-origin allow-modals allow-top-navigation allow-downloads\" allowscripts=\"1\" allowdownloads=\"1\" allowfullscreen=\"1\" frameborder=\"0\" id=\"viewer\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbx69GPoJtf9sSevsUbWtPr46vpa01u4oNkHjFmkkWxmj62AZ0q-\/exec?&export=view&field="+titlex+"&base="+viewno+"\"><noscript>&nbsp;Enable Javascript to Load File<\/noscript><\/iframe>");
 				win.document.write("</body></html>"); 	 
 				win.document.title = unescape(titlex);
-			}
+			//}
 		}
 		catch(err)
 		{
@@ -280,14 +280,14 @@ function pdf(input, viewno)
 {
   
 var titlex = input.innerHTML;
-var vb = viewno+btoa(titlex.replace(/\ /g, "_").substring(0,4))
+/*var vb = viewno+btoa(titlex.replace(/\ /g, "_").substring(0,4))
 var vy = SHA256(vb.trim())
 var vx = vy.toUpperCase().charCodeAt(0)-32
 /*	
 	
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { */
-	 window.open("https://thsconline.github.io/s/v" + vx + "/" +viewno+"/"+titlex);	    
+	 window.open("https://thsconline.github.io/s/v/" +viewno+"/"+titlex);	    
 /*  }
     else
     {
