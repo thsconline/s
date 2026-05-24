f276705066bd05f1977feb8491e5f44f9b6e291d
thsconline <thsconline@users.noreply.github.com>
2026-05-24 20:43:50 +1000

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -78,7 +78,111 @@ function loadshell()
 	{
 		window.location = "/s/fz/"
 	}
-	break;			
+	break;		
+
+	case "em":
+		try
+		{
+			var embedUrl;
+			var titlex;
+			
+			try
+			{
+			var raw = decodeURIComponent(url.split("/s/em/")[1]);
+			if(!raw) throw new Error("Missing path");
+			if(!raw.toLowerCase().endsWith(".pdf")) raw += ".pdf";
+			var parts = raw.split("/");
+			var file = parts.pop();
+			titlex = file.replace(/\.pdf$/i, "");
+			var path = parts.join("/");
+			embedUrl = "/" + (path ? path + "/" : "") + file;
+			}
+			catch(err)
+			{
+			console.log("Embed error:", err);
+			titlex = "File not found";
+			embedUrl = "/s/index/404_html.pdf";
+			}
+
+			
+				if(window.self !== window.top)
+				{
+					win=window.open("about:blank","_blank");
+					if (window.focus) {win.focus()}
+				}
+				else
+				{
+					win=window.open("about:blank","_self");
+					if (window.focus) {win.focus()}		
+				}
+				win.document.write("<html><head><title>"+titlex+"</title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\">");
+				win.document.write("<meta http-equiv=\"content-type\" content=\"text\/html; charset=utf-8\"><link rel=\"shortcut icon\" type=\"image\/x-icon\" href=\"https:\/\/thsconline.github.io\/s\/images\/icon_pdf2.png\">");
+				win.document.write("<link href=\"\/s\/styles.css\" rel=\"stylesheet\" type=\"text\/css\">");
+				win.document.write("<style>html, body {height:100% !important;}</style>");
+				win.document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
+				win.document.write("<\/head><body>");			
+				win.document.write("<div id=\"overlaybar\" style=\"z-index:1000\; width:100%;\">"+ unescape(titlex) +"<span id=\"overlayinsert\" style=\"float:right !Important\"><a target=\"_blank\" download href=\"https://thsconline.github.io" + embedUrl+"\" class=\"border\">Download File<\/a>&nbsp;&nbsp;<a class=\"border\" href=\"#v\" onclick=\"window.close()\">Close &#215;</span></div><br>")
+				win.document.write("<iframe style=\"width:100%; height:96%;\" height=\"96%\" sandbox=\"allow-scripts allow-popups allow-pointer-lock allow-presentation allow-same-origin allow-modals allow-top-navigation allow-downloads\" allowscripts=\"1\" allowdownloads=\"1\" allowfullscreen=\"1\" frameborder=\"0\" id=\"viewer\" src=\"https:\/\/thsconline.github.io\/pdf\/viewer.html?file="+embedUrl+"\"><noscript>&nbsp;Enable Javascript to Load File<\/noscript><\/iframe>");
+				win.document.write("</body></html>"); 	 
+				win.document.title = unescape(titlex);
+		}
+		catch(err)
+		{
+			window.location = "/s/"
+		}
+	break;
+
+	case "em_standalone":
+		try
+		{
+			var embedUrl;
+			var titlex;
+			
+			try
+			{
+			var raw = decodeURIComponent(url.split("/s/em/")[1]);
+			if(!raw) throw new Error("Missing path");
+			if(!raw.toLowerCase().endsWith(".pdf")) raw += ".pdf";
+			var parts = raw.split("/");
+			var file = parts.pop();
+			titlex = file.replace(/\.pdf$/i, "");
+			var path = parts.join("/");
+			embedUrl = "/" + (path ? path + "/" : "") + file;
+			}
+			catch(err)
+			{
+			console.log("Embed error:", err);
+			titlex = "File not found";
+			embedUrl = "/s/index/404_html.pdf";
+			}
+
+			
+				if(window.self !== window.top)
+				{
+					win=window.open("about:blank","_blank");
+					if (window.focus) {win.focus()}
+				}
+				else
+				{
+					win=window.open("about:blank","_self");
+					if (window.focus) {win.focus()}		
+				}
+				win.document.write("<html><head><title>"+titlex+"</title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\">");
+				win.document.write("<meta http-equiv=\"content-type\" content=\"text\/html; charset=utf-8\"><link rel=\"shortcut icon\" type=\"image\/x-icon\" href=\"https:\/\/thsconline.github.io\/s\/images\/icon_pdf2.png\">");
+				win.document.write("<link href=\"\/s\/styles.css\" rel=\"stylesheet\" type=\"text\/css\">");
+				win.document.write("<style>html, body {height:100% !important;}</style>");
+				win.document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
+				win.document.write("<\/head><body>");
+				win.document.write("<iframe style=\"width:100%; height:96%;\" height=\"96%\" sandbox=\"allow-scripts allow-popups allow-pointer-lock allow-presentation allow-same-origin allow-modals allow-top-navigation allow-downloads\" allowscripts=\"1\" allowdownloads=\"1\" allowfullscreen=\"1\" frameborder=\"0\" id=\"viewer\" src=\"https:\/\/thsconline.github.io\/pdf\/viewer.html?file="+embedUrl+"\"><noscript>&nbsp;Enable Javascript to Load File<\/noscript><\/iframe>");
+				win.document.write("</body></html>"); 	 
+				win.document.title = unescape(titlex);
+		}
+		catch(err)
+		{
+			window.location = "/s/"
+		}
+	break;
+			
 	case "fz":	
 		try
 		{
@@ -166,6 +270,10 @@ function loadshell()
 		window.location = "/s/"
 	}
 	break;
+
+	
+
+			
 	case "v_standalone":
 	var viewno = url.split("/s/v_standalone/")[1].split("/")[0]
 	var titlex = url.split("/s/v_standalone/")[1].split("/")[1]		
@@ -222,7 +330,7 @@ function loadshell()
 				win.document.write("<style>html, body {height:100% !important;}</style>");
 				win.document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
 				win.document.write("<\/head><body>");			
-				win.document.write("<div id=\"overlaybar\" style=\"z-index:1000\; width:100%;\">"+ unescape(titlex) +"<span id=\"overlayinsert\" style=\"float:right !Important\"><a target=\"blank\" href=\"https://thsconline.github.io/s/d/"+viewno+"/"+titlex+"\" class=\"border\">Download File<\/a>&nbsp;&nbsp;<a class=\"border\" href=\"#v\" onclick=\"window.close()\">Close &#215;</span></div><br>")
+				win.document.write("<div id=\"overlaybar\" style=\"z-index:1000\; width:100%;\">"+ unescape(titlex) +"<span id=\"overlayinsert\" style=\"float:right !Important\"><a target=\"_blank\" href=\"https://thsconline.github.io/s/d/"+viewno+"/"+titlex+"\" class=\"border\">Download File<\/a>&nbsp;&nbsp;<a class=\"border\" href=\"#v\" onclick=\"window.close()\">Close &#215;</span></div><br>")
 				win.document.write("<iframe style=\"width:100%; height:96%;\" height=\"96%\" sandbox=\"allow-scripts allow-popups allow-pointer-lock allow-presentation allow-same-origin allow-modals allow-top-navigation allow-downloads\" allowscripts=\"1\" allowdownloads=\"1\" allowfullscreen=\"1\" frameborder=\"0\" id=\"viewer\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbx69GPoJtf9sSevsUbWtPr46vpa01u4oNkHjFmkkWxmj62AZ0q-\/exec?&export=view&field="+titlex+"&base="+viewno+"\"><noscript>&nbsp;Enable Javascript to Load File<\/noscript><\/iframe>");
 				win.document.write("</body></html>"); 	 
 				win.document.title = unescape(titlex);
