60501ee930ee5b5c0158a8664fdf563506cd3af9
thsconline <thsconline@users.noreply.github.com>
2026-05-24 20:23:36 +1000

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -282,10 +282,12 @@ function loadshell()
 	break;
 	}
 }
-
+/*
 function pdf(input, viewno)
 {
-  
+
+
+	
   
 
 var titlex = input.innerHTML;
@@ -363,6 +365,281 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
   /*  }
     return false;*/
 	}
+}
+
+async function pdf(input, viewno)
+{
+
+var titlex = input.innerHTML.trim();
+
+var url = window.location.search.substring(1);
+var urlParams = new URLSearchParams(url);
+
+var bulkdownload = urlParams.get("download");
+var legacymode = urlParams.get("legacy");
+
+var pdfSelection =
+	urlParams.get("pdfSelection");
+
+var pdfSelectionEnabled =
+	pdfSelection == "1" ||
+	pdfSelection == "true";
+
+var forceChoice =
+	/[05]$/.test(viewno.toString());
+
+var usepdfSelection =
+	forceChoice || pdfSelectionEnabled;
+
+// unique pdfSelection id
+var pdfSelectionId =
+	"pdfpdfSelection_" +
+	viewno +
+	"_" +
+	Date.now() +
+	"_" +
+	Math.floor(Math.random() * 100000);
+
+var isMobile =
+	/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+	||
+	/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4));
+
+function launchResource(resource, mode)
+{
+	var finalurl =
+		resource.pdfData ||
+		(
+			mode == "download"
+			?
+			"https://thsconline.github.io/s/d/" + viewno + "/" + titlex
+			:
+			"https://thsconline.github.io/s/v/" + viewno + "/" + titlex
+		);
+
+	if(mode == "download")
+	{
+		if(
+			(legacymode == "true" || legacymode == "1")
+			&&
+			!isMobile
+		)
+		{
+			var i = document.createElement('iframe');
+
+			i.sandbox = "allow-scripts allow-downloads";
+			i.style.display = 'none';
+			i.src = finalurl;
+
+			document.body.appendChild(i);
+		}
+		else
+		{
+			window.open(finalurl);
+		}
+	}
+	else
+	{
+		window.open(finalurl);
+	}
+}
+
+var resources = null;
+
+try
+{
+	const jsonUrl =
+		"https://thsconline.github.io/s/index/" +
+		viewno +
+		".json";
+
+	const response = await fetch(jsonUrl);
+
+	if(response.ok)
+	{
+		const jsonData = await response.json();
+
+		if(jsonData[titlex])
+		{
+			resources = jsonData[titlex];
+
+			if(!Array.isArray(resources))
+			{
+				resources = [resources];
+			}
+		}
+	}
+}
+catch(err)
+{
+	console.log(err);
+}
+
+if(!resources)
+{
+	if(bulkdownload == "true" || bulkdownload == "1")
+	{
+		launchResource({}, "download");
+	}
+	else
+	{
+		launchResource({}, "view");
+	}
+
+	return;
+}
+
+var defaultItem =
+	resources.find(x => x.default === true);
+
+if(!defaultItem)
+{
+	defaultItem = resources[0];
+}
+
+if(resources.length <= 1 || !usepdfSelection)
+{
+	if(bulkdownload == "true" || bulkdownload == "1")
+	{
+		launchResource(defaultItem, "download");
+	}
+	else
+	{
+		launchResource(defaultItem, "view");
+	}
+
+	return;
+}
+
+var selectionElement = document.createElement("div");
+
+selectionElement.id = pdfSelectionId;
+
+selectionElement.style.position = "fixed";
+selectionElement.style.top = "50%";
+selectionElement.style.left = "50%";
+selectionElement.style.transform = "translate(-50%, -50%)";
+selectionElement.style.background = "#fff";
+selectionElement.style.padding = "20px";
+selectionElement.style.border = "1px solid #ccc";
+selectionElement.style.zIndex = "999999";
+selectionElement.style.minWidth = "320px";
+selectionElement.style.maxWidth = "90%";
+selectionElement.style.boxShadow = "0 0 15px rgba(0,0,0,0.4)";
+
+var countdown = 3;
+
+selectionElement.innerHTML =
+	"<h3 style='margin-top:0'>" + titlex + "</h3>" +
+	"<div id='" + pdfSelectionId + "_links'></div>" +
+	"<p>" +
+	"Loading default resource in " +
+	"<span id='" + pdfSelectionId + "_countdown'>" +
+	countdown +
+	"</span>s" +
+	"</p>" +
+	"<button id='" + pdfSelectionId + "_cancel'>Cancel</button>";
+
+document.body.appendChild(selectionElement);
+
+var linksDiv =
+	document.getElementById(
+		pdfSelectionId + "_links"
+	);
+
+resources.forEach(function(resource)
+{
+	var a = document.createElement("a");
+
+	a.href = "#";
+
+	a.style.display = "block";
+	a.style.marginBottom = "10px";
+
+	a.innerHTML =
+		resource.title ||
+		resource.pdfData;
+
+	a.onclick = function(e)
+	{
+		e.preventDefault();
+
+		clearInterval(timer);
+
+		try
+		{
+			selectionElement.remove();
+		}
+		catch(err)
+		{
+		}
+
+		if(bulkdownload == "true" || bulkdownload == "1")
+		{
+			launchResource(resource, "download");
+		}
+		else
+		{
+			launchResource(resource, "view");
+		}
+	};
+
+	linksDiv.appendChild(a);
+});
+
+document.getElementById(
+	pdfSelectionId + "_cancel"
+).onclick = function()
+{
+	clearInterval(timer);
+
+	try
+	{
+		selectionElement.remove();
+	}
+	catch(err)
+	{
+	}
+};
+
+var timer = setInterval(function()
+{
+	countdown--;
+
+	var el =
+		document.getElementById(
+			pdfSelectionId + "_countdown"
+		);
+
+	if(el)
+	{
+		el.innerHTML = countdown;
+	}
+
+	if(countdown <= 0)
+	{
+		clearInterval(timer);
+
+		try
+		{
+			selectionElement.remove();
+		}
+		catch(err)
+		{
+		}
+
+		if(bulkdownload == "true" || bulkdownload == "1")
+		{
+			launchResource(defaultItem, "download");
+		}
+		else
+		{
+			launchResource(defaultItem, "view");
+		}
+	}
+
+}, 1000);
+
 }
 
 String.prototype.capitalize = function(){
